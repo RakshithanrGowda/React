d5201bc8f12c0b43343b8b779111aceda674d4b4
RakshithanrGowda <rakshithanr342@gmail.com>
2024-08-19 10:10:28 +0530

    React first comment

---
+++ App.js
@@ -0,0 +1,15 @@
+/*const heading = React.createElement("h1", {id:"heading"}, "Hello World from Raksh");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);*/
+const parent =React.createElement("div",{id:"parent"},[
+    React.createElement("div",{id:"child1"},[
+        React.createElement("h1",{id:"Heading 1"},"Heading 1"),
+        React.createElement("h2",{id:"Heading 2"},"Heading 2"),
+ ]),
+ React.createElement("div",{id:"child2"},[
+    React.createElement("h3",{id:"Heading 3"},"Heading 3"),
+    React.createElement("h4",{id:"Heading 4"},"Heading 4"),
+]),
+]);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
